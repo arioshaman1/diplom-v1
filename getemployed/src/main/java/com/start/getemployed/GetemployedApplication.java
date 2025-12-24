88994d6c8f2eac5408df31c28564a67d30dcd433
arioshaman1 <adaikin.andr321@gmail.com>
2025-12-24 03:01:23 +0300

    first commit

---
+++ getemployed/src/main/java/com/start/getemployed/GetemployedApplication.java
@@ -0,0 +1,13 @@
+package com.start.getemployed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetemployedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetemployedApplication.class, args);
+	}
+
+}
